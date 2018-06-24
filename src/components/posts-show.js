3191e6fae46acd85cce55161a2ef26d5eb9a888d
Anthony Parrett <anthonyparrett7@gmail.com>
2018-06-23 22:58:40 -0500

    A user can delete a post

--- src/components/posts-show.js
+++ src/components/posts-show.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { fetchPost } from '../actions';
+import { Link } from 'react-router-dom';
+import { fetchPost, deletePost } from '../actions';
 
 class PostsShow extends Component {
   componentDidMount() {
     const { id } = this.props.match.params;
     this.props.fetchPost(id);
+  }
+
+  handleDeleteClick() {
+    const { deletePost, post : { id } } = this.props;
+    deletePost(id, () => {
+      this.props.history.push('/');
+    });
   }
 
   render() {
@@ -17,9 +25,13 @@ class PostsShow extends Component {
 
     return (
       <div> 
+        <Link to="/">Back to Index</Link>
         <h3>{post.title}</h3>
         <h6>Categories: {post.categories}</h6>
         <p>{post.content}</p>
+        <button className="btn btn-danger" onClick={this.handleDeleteClick.bind(this)}>Delete</button>
+        <div>
+        </div>
       </div>
     );
   }
@@ -29,4 +41,4 @@ function mapStateToProps({ posts }, ownProps) {
   return { post: posts[ownProps.match.params.id] };
 }
 
-export default connect(mapStateToProps, { fetchPost })(PostsShow);
+export default connect(mapStateToProps, { fetchPost, deletePost })(PostsShow);
